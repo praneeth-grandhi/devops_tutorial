7e35b375caaecf0220a71761b8117743950a304a
Praneeth Grandhi <praneeth.gsk@gmail.com>
2026-01-20 18:04:56 +0530

    feat: add CI pipeline, fixed syntax error in test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 console.log("Starting tests...");
 setTimeout(() => console.log("Waiting 3 seconds.."), 3000);
-console.log("Tests completed.");g('Hello, DevOps');
+console.log("Tests completed.");
 console.log('I am learning CI/CD pipelines!');
